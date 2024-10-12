a9f20a65fd367e0628f8b03b1f88f382e6eaa5a3
Gourav <gouravsingla830@gmail.com>
2024-10-12 22:49:10 +0530

    initial commmit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+interface PageProps {
+  
+}
+
+interface PageState {
+  
+}
+
+class Page extends Component<PageProps, PageState> {
+  render() {
+    return (
+      <div>
+          Home page Work in progrress
+      </div>
+    );
+  }
+}
+
+export default Page;
