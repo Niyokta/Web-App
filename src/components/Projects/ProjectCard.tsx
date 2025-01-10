d60bb0bd54478ea4f5c4d5ad25ca5ec62d731ed9
gourav830 <gouravsingla830@gmail.com>
2025-01-10 21:53:44 +0530

    little changes

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { FaInfoCircle } from 'react-icons/fa';
+import { FC } from "react";
+import { FaInfoCircle } from "react-icons/fa";
 
 interface Project {
   title: string;
@@ -16,13 +16,33 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
   const { title, client_name, client_country, min_budget } = project;
 
   return (
-    <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px', width: '525px' }}>
-      <h2 style={{ maxHeight: '100px', overflow: 'hidden', textOverflow: 'ellipsis' }}>{title}</h2>
+    <div
+      style={{
+        padding: "20px",
+        width: "520px",
+      }}
+      className="hover:bg-[#f7f7f7] rounded-md shadow-md shadow-[#eeeeee]"
+    >
+      <h2
+        style={{
+          maxHeight: "100px",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+      >
+        {title}
+      </h2>
       <p>
         <strong>{client_name}</strong> ({client_country})
       </p>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <span>Minimum Data: {min_budget}</span>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <span>Minimum Bid: {min_budget}</span>
         <FaInfoCircle size={20} color="gray" />
       </div>
     </div>
