a402fa740de3b2c9178ba87a4f49af1a983c52a9
gourav830 <gouravsingla830@gmail.com>
2025-01-11 15:38:02 +0530

    little changes

--- src/app/(project-show)/project/[project_id]/page.tsx
+++ src/app/(project-show)/project/[project_id]/page.tsx
@@ -1,26 +1,29 @@
-'use client'
-import { useParams } from 'next/navigation'; 
-import ProjectDetail from '@/components/Projects/projectDetails';
-import { useEffect, useState } from 'react';
-
+"use client";
+import { useParams } from "next/navigation";
+import ProjectDetail from "@/components/Projects/projectDetails";
+import { useEffect, useState } from "react";
+export type ResponseData = {
+  project?: any;
+  error?: string;
+};
 const Page = () => {
   const params = useParams<{ projectID: string }>();
-  const   projectID  = params; 
+  const projectID = params;
   const [response, setResponse] = useState<ResponseData | null>(null);
   const [loading, setLoading] = useState(true);
   // console.log(projectID);
   useEffect(() => {
     const fetchProjectDetails = async () => {
       try {
-        const res = await fetch('/api/Project/username/project', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
+        const res = await fetch("/api/Project/username/project", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ projectID }),
         });
         const data = await res.json();
         setResponse(data);
       } catch (error) {
-        setResponse({ error: 'Failed to fetch project details' });
+        setResponse({ error: "An error occurred", project: null });
       } finally {
         setLoading(false);
       }
@@ -31,10 +34,15 @@ const Page = () => {
     }
   }, [projectID]);
 
-
   return (
     <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : response?.error ? (
+        <p>{response.error}</p>
+      ) : (
         <ProjectDetail project={response?.project} />
+      )}
     </div>
   );
 };
