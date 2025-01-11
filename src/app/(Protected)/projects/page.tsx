46e906b0fc1886fcc371016240297d02cc9ed0c8
gourav830 <gouravsingla830@gmail.com>
2025-01-12 01:02:58 +0530

    created projects route for signed in user

--- src/app/(Protected)/projects/page.tsx
+++ src/app/(Protected)/projects/page.tsx
@@ -1,17 +1,79 @@
-
-'use client';
-import ProjectUser from '@/components/Projects/projectUser';
+'use client'
 import React from 'react';
-// import ProjectUser from '@/components/Projects/ProjectUser';
-
+import ProjectCard from '@/components/Projects/ProjectCard';
+import { Checkbox } from "@/components/ui/checkbox"
+import { AllProjectsLoader } from '@/components';
 export default function Projects() {
-  return (
-    <div>
-      <h1>Here you will see all the projects</h1>
-      <p>Project User:</p>
-      <ProjectUser />
-    </div>
-  );
-
-}
+const [loading,setloading]=React.useState<true | false>(true)
+  const [projects, setProjects] = React.useState([{
+    project_id:0,
+    title:"",
+    client_name:"",
+    client_country:"",
+    min_budget:"",
+    skills_required:[],
+    category:[]
+  }]);
+    const [filter,setfilter]=React.useState<string[]>([])
   
+    React.useEffect(() => {
+      async function fetchProjects() {
+        try {
+          const response = await fetch('api/Project/userProject',{
+            method: 'GET',
+            credentials:"include"
+          });
+          console.log(response);
+          const data = await response.json();
+          if(data) setloading(false);
+          setProjects(data.projects); 
+        } catch (error) {
+          console.error('Failed to fetch projects:', error);
+        }
+      }
+  
+      fetchProjects();
+    }, []);
+    return (
+      loading?(
+        <AllProjectsLoader/>
+      ):
+      <div>
+        <div className='w-full h-[100px] flex items-center justify-end'>
+          <Checkbox className='w-[15px] h-[15px]' onCheckedChange={()=>{
+            if(filter.includes("Web Development")){
+              const temp=filter.filter(s=>s!="Web Development");
+              setfilter(temp);
+            }
+            else{
+              setfilter([...filter,"Web Development"])
+            }
+          }}/> <p className='px-[10px] font-light text-[13px]'>Web Development</p>
+          <Checkbox className='w-[15px] h-[15px]' onCheckedChange={()=>{
+            if(filter.includes("Graphic Design")){
+              const temp=filter.filter(s=>s!="Graphic Design");
+              setfilter(temp);
+            }
+            else{
+              setfilter([...filter,"Graphic Design"])
+            }
+          }}/> <p className='px-[10px] font-light text-[13px]'>Graphic Design</p>
+          <Checkbox className='w-[15px] h-[15px]' onCheckedChange={()=>{
+            if(filter.includes("Android Development")){
+              const temp=filter.filter(s=>s!="Android Development");
+              setfilter(temp);
+            }
+            else{
+              setfilter([...filter,"Android Development"])
+            }
+          }}/> <p className='px-[10px] font-light text-[13px]'>Android Development</p>
+        </div>
+       
+        <div className='grid grid-flow-row grid-col-1 md:grid-cols-2 gap-10'>
+          {projects.map((project,index) => (
+            <ProjectCard key={index} project_id={project.project_id} filter={filter} skills={project.skills_required} category={project.category} title={project.title} client_name={project.client_name} client_country={project.client_country} min_budget={project.min_budget} />
+          ))}
+        </div>
+      </div>
+    );
+  }
--- src/app/api/Project/userProject/route.ts
+++ src/app/api/Project/userProject/route.ts
@@ -12,21 +12,33 @@ export async function GET(request: Request) {
         message: "No token found"
       })
     }
-    const response = await fetch('http://3.6.34.255:3000/api/v1/project/getProjects', {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'content-type': 'application/json',
-        'authorization': accessToken
-
-      }, body: JSON.stringify({
-        cleintID:'12'   
-        
-    })
-    })
-    const res = await response.json();
-    console.log(res);
-    return Response.json(res);
+    const userResponse=await fetch('http://3.6.34.255:3000/api/v1/user/getuser',{
+      method:'GET',
+      credentials:'include',
+      headers:{
+          'X-Client-Type':'mobile',
+          'authorization':accessToken
+      }
+  })
+  // console.log("user - Response         : ",userResponse);
+  const userDetails = await userResponse.json();
+  // console.log("userResponse : ",userDetails.user);
+  // console.log(userDetails.user.projects);
+    // const response = await fetch('http://3.6.34.255:3000/api/v1/project/getProjects', {
+    //   method: "POST",
+    //   credentials: "include",
+    //   headers: {
+    //     "content-type": "application/json",
+    //     "credentials":"include"
+    //   },
+    //   body: JSON.stringify({
+    //     clientID : userDetails.user.userDetails
+    //   })
+    // })
+    // console.log("Response:         : ",response)
+    // const res = await response.json();
+    // console.log("JSon response :      : ",res);
+    return Response.json(userDetails.user);
   } catch (err: any) {
     return Response.json({ status: "400", message: err.message });
   }
